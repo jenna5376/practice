d8e05ed78102df820704244e8415543a36d6baf3
jenna5376 <jenna5376@gmail.com>
2022-10-31 18:53:34 -0400

    form animation

--- form/script.js
+++ form/script.js
@@ -1,4 +1,5 @@
-document.getElementById("submit").addEventListener('click', onSubmit);
+let submitButton = document.getElementById("submit")
+submitButton.addEventListener('click', onSubmit);
 
 function onSubmit(){
     let nameBox = document.getElementById("name-input");
@@ -10,7 +11,25 @@ function onSubmit(){
     let schoolBox = document.getElementById("school-input");
     console.log(schoolBox.value);
 
+    let textBox = document.getElementById("text");
+    textBox.innerText = `Thank you ${nameBox.value}`;
+
+    submitButton.style.backgroundColor = "light-grey";
+
+    document.getElementById("form").style.transform = "rotate(24deg)";
+    document.getElementById("form").style.transition = "transform 0.5s ease-in";
+
     setTimeout(() => {
+        const inputs = document.querySelectorAll('#name-input, #address-input, #age-input, #school-input');
+        inputs.forEach(input => {
+            input.value = "";
+        })        
+        textBox.innerText = "";
+        submitButton.style.backgroundColor = "white";
+        document.getElementById("form").style.transform = "rotate(0deg)";
 
     }, 2000)
 }
+
+document.getElementById(p);
+p.innerText = 'Thank you ${namebox.value}';
