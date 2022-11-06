0065d2a871ddf1be16ebeebd132e3e544376f3b0
jenna5376 <jenna5376@gmail.com>
2022-11-06 01:29:58 -0500

    cleaned up code

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,17 +1,10 @@
-//https://www.youtube.com/watch?v=kzHmtmCMUwk&ab_channel=CodeCreative
-
 const addForm = document.querySelector('.add');
 const list = document.querySelector(".list-group")
 
-console.log(addForm);
-
 addForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    //add is the input name
-    //trim to remove whitespace
-    const todo = addForm.add.value.trim();
 
-    //checks if there is input
+    const todo = addForm.task.value.trim();
     if (todo.length != 0){
         generateTemplate(todo);
         addForm.reset();
@@ -29,7 +22,6 @@ const generateTemplate = (todo) => {
     list.innerHTML += html;
 }
 
-//if the element you are clicing says delete, delete it
 list.addEventListener("click", (e) => {
     if (e.target.classList.contains("delete")){
         e.target.parentElement.remove();
